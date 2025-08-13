d5d72114d305a39f23984acab0811c3f74363078
Greider <kotx25@yandex.ru>
2025-08-13 23:04:51 +0500

    Initial commit

---
+++ src/main/java/ru/academy/homework/motoshop/MotoShopApplication.java
@@ -0,0 +1,13 @@
+package ru.academy.homework.motoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotoShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/academy/homework/motoshop/MotoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.academy.homework.motoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
